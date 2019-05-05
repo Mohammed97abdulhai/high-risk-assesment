7bf70561094fc7580f69cf2b175e2fefb1a10820
Mohammed97abdulhai <37939118+Mohammed97abdulhai@users.noreply.github.com>
2019-05-05 15:56:25 +0300

    added the rest of the rules and orgnized the input/output

--- homework.py
+++ homework.py
@@ -1,6 +1,6 @@
 from pyknow import *
 from enum import Enum
-from PyInquirer import *
+from PyInquirer import prompt
 
 
 class Nationality(Fact):
@@ -16,6 +16,9 @@ class InquiredByCML(Fact):
     pass
 
 class Beneficiary(Fact):
+    pass
+
+class calculateTotalRisk(Fact):
     pass
 
 class CusomerType(Enum):
@@ -53,10 +56,6 @@ class RiskFactor():
         self.value = value
         self.weight = weight
 
-class RisedualRisk():
-    risedual_nationality = 0
-    risedual_business = 0
-    risedual_transaction =0
 
 def calculate_the_risk(reputaion , financial_legal , operational , adherence , control_system , procedures):
     inherent_risk = ((reputaion.value * reputaion.weight + financial_legal.value * financial_legal.weight + operational.value * operational.weight)
@@ -68,15 +67,40 @@ def calculate_the_risk(reputaion , financial_legal , operational , adherence , c
     risk = (inherent_risk + (2*(100 - control_risk)))/3
     return risk
 
+maxx = 15
+class RiskValue(Enum):
+    high = maxx/3
+    medium = (maxx/3)/2
+    low = 0
+
+
+
+class MainFactors():
+    nationality_risk = RiskValue.low
+    business_nature_risk = RiskValue.low
+    transaction_risk = RiskValue.low
+
+
+
 class InferenceEngine(KnowledgeEngine):
     #another dumb shit i think but i don't know any other way to represent it and i don't think the deffacts can do the same thing i am trying to do
     high_risk_nations = ["afghanistan","algeria","argentina","bahrain","brazil","china","colombia","cuba","djibouti","egypt","equatorial guinea","gibraltar","greece",
                             "india","indonesia","iran","iraq","lebanon","korea DPR","kuwait"]
     high_risk_business = ["lawyer","accountant","broker"]
     medium_risk_business = ["real estate","jewlery","cars"]
+
+    @Rule(salience = 7)
+    def startuprule(self):
+        question = [
+            {'type' : 'confirm',
+             'message' : 'is the user inquired by CML?',
+             'name' : 'inquired'}]
+        inquired_answer = prompt(question)
+        if inquired_answer['inquired']:
+            self.declare(InquiredByCML())
     
     #high risk nations rule
-    @Rule(Nationality(P(lambda x: str(x) in InferenceEngine.high_risk_nations)))
+    @Rule(Nationality(P(lambda x: str(x) in InferenceEngine.high_risk_nations)),salience = 2)
     def fuckogg(self):
         #calculate the inherent risk 
         reputaion = RiskFactor(80,100)
@@ -91,10 +115,17 @@ class InferenceEngine(KnowledgeEngine):
         #calculate the overall risk
         risk = calculate_the_risk(reputaion,financial_legal,operational,adherence,control_system,procedures)
         print("nationality risk:",risk)
+        if(risk >=23 and risk <= 35):
+            MainFactors.nationality_risk = RiskValue.low
+        elif(risk >= 36 and risk <=55):
+            MainFactors.nationality_risk = RiskValue.medium
+        elif(risk >=56 and risk <=73):
+            MainFactors.nationality_risk = RiskValue.high
+        print(MainFactors.nationality_risk)
         
 
     #high risk business rule
-    @Rule(BusinessNature(P(lambda nature: str (nature) in InferenceEngine.high_risk_business)))
+    @Rule(BusinessNature(P(lambda nature: str (nature) in InferenceEngine.high_risk_business)),salience=2)
     def fukcthishist(self):
 
         #calculate the inherent risk
@@ -111,8 +142,16 @@ class InferenceEngine(KnowledgeEngine):
         risk = calculate_the_risk(reputaion1,finanical_legal1,operational1,adherence1,control_system1,procedures1)
         print("business risk:",risk)
 
+        if(risk >=23 and risk <= 35):
+            MainFactors.business_nature_risk = RiskValue.low
+        elif(risk >= 36 and risk <=55):
+            MainFactors.business_nature_risk = RiskValue.medium
+        elif(risk >=56 and risk <=73):
+            MainFactors.business_nature_risk = RiskValue.high
+        print(MainFactors.business_nature_risk)
+
     #medium risk business rule
-    @Rule(BusinessNature(P(lambda nature : str (nature) in InferenceEngine.medium_risk_business)))
+    @Rule(BusinessNature(P(lambda nature : str (nature) in InferenceEngine.medium_risk_business)),salience=2)
     def sososo(self):
         #assign the corresponding value of the  inherent risk factors
         reputaion = RiskFactor(50,100)
@@ -128,12 +167,28 @@ class InferenceEngine(KnowledgeEngine):
         #calulate the overall risk
         risk = calculate_the_risk(reputaion,finanical_legal,operational,adherence,control_system,procedures)
         print("business risk: ",risk)
-
-
+        
+        if(risk >=23 and risk <= 35):
+            MainFactors.business_nature_risk = RiskValue.low
+        elif(risk >= 36 and risk <=55):
+            MainFactors.business_nature_risk = RiskValue.medium
+        elif(risk >=56 and risk <=73):
+            MainFactors.business_nature_risk = RiskValue.high
+        print(MainFactors.business_nature_risk)
+    
     # withdrawal limit rule
-    @Rule(Transaction(MATCH.trans ,  MATCH.threshold))
-    def trans_limit_exceeded(self,trans, threshold):
-        if(trans > threshold):
+    @Rule(Transaction(MATCH.withdrawal_list , MATCH.deposit_list, MATCH.withdrawal_threshold, MATCH.deposit_threshold),salience=2)
+    def trans_limit_exceeded(self,withdrawal_list,deposit_list, withdrawal_threshold, deposit_threshold):
+        withdrawal_checker = False
+        deposit_checker = False
+        for i in withdrawal_list : 
+            if i > withdrawal_threshold:
+                withdrawal_checker = True
+                break
+        for i in deposit_list :
+            if i > deposit_threshold:
+                deposit_checker = True
+        if deposit_checker or withdrawal_checker :
             reputaion = RiskFactor(50,100)
             financial_legal = RiskFactor(80,80)
             operational = RiskFactor(80,60)
@@ -145,14 +200,36 @@ class InferenceEngine(KnowledgeEngine):
             risk = calculate_the_risk(reputaion, financial_legal,operational,
                     adherence,control_system,procedures)
             print("transactions risk:",risk)
+            
+            if(risk >=23 and risk <= 35):
+                MainFactors.business_nature_risk = RiskValue.low
+            elif(risk >= 36 and risk <=55):
+                MainFactors.business_nature_risk = RiskValue.medium
+            elif(risk >=56 and risk <=73):
+                MainFactors.business_nature_risk = RiskValue.high
 
-    @Rule(EXISTS(InquiredByCML()))
+            print(MainFactors.transaction_risk)
+            self.declare(calculateTotalRisk("yes"))
+
+    @Rule(EXISTS(InquiredByCML()),salience =6)
     def inquiredbycml(self):
-        print("high risk")
+        print("high risk inq")
+        self.reset()
 
     @Rule(Beneficiary(),salience = 6)
     def beneficiary(self):
         print("high risk")
+        self.reset()
+    @Rule(calculateTotalRisk("yes"),salience = 1)
+    def cal(self):
+        total = (MainFactors.nationality_risk).value + (MainFactors.transaction_risk).value + (MainFactors.business_nature_risk).value
+        print(total)
+        if total >=0 and total <= maxx/3:
+            print('low risk')
+        elif total > maxx/3 and total <= (2* (maxx/3)):
+            print('medium risk')
+        elif total> (2* (maxx/3)) and total < maxx:
+            print('high risk')
 
 questions = [
     {
@@ -269,7 +346,6 @@ questions = [
         #TODO :validate it's a number
     },
     {
-        
         'type': 'confirm',
         'message': 'is your the beneficiary?',
         'name': 'beneficiary',
@@ -285,10 +361,54 @@ customer.set_personal_info(answers['first_name'],answers['last_name'], answers['
 answers['country'],answers['city'],answers['nationality'],answers['gender'],97,"single")
 customer.set_customer_information(CusomerType.individiual, answers['branch'],answers['business_nature'],"nothing",int(answers['average_yearly_income']),
 int(answers['deposit_threshold']),int(answers['withdrawal_threshold']), answers['beneficiary'])
+questions1 = [
+    {
+        'type' : 'confirm',
+        'message' : 'do you want to make transaction',
+        'name' : 'transaction',
+        'default' : True,
+    }
+]
+answers1 = prompt(questions1)
+trans = answers1['transaction']
+deposit_transaction = []
+withdrawal_transaction = []
+while trans is True :
+    ask_about_trans = [
+        {
+            'type' : 'checkbox',
+            'message' : 'what type of transaction do you want',
+            'name' : 'transaction_selection',
+            'choices' : [
+            {
+                'name' : 'deposit',
+                'checked' : True
+            },
+            {
+                'name' : 'withdrawal'
+            }
+        ],
+        }
+    ]
+    trans_answers = prompt(ask_about_trans)
+    trans_amount = int(input("enter the amount of your transaction:"))
+    print(trans_answers)
+    if trans_answers['transaction_selection'][0] == 'deposit':
+        deposit_transaction.append(trans_amount)
+    else :
+        withdrawal_transaction.append(trans_amount)
 
-user_trans = int(input("enter  your transaction:"))
+    answers = prompt(questions1)
+    trans = answers['transaction']
+
+print(withdrawal_transaction)
+print(deposit_transaction)
 inferenceEngine = InferenceEngine()
 inferenceEngine.reset()
-inferenceEngine.declare(Nationality(customer.nationality),Beneficiary(),BusinessNature(customer.business_nature),
-Transaction(user_trans, customer.withdrawal_threshold))
+inferenceEngine.declare(Nationality(customer.nationality),BusinessNature(customer.business_nature),
+Transaction(withdrawal_transaction, deposit_transaction, customer.withdrawal_threshold, customer.deposit_threshold))
+
+if customer.is_beneficiary:
+    inferenceEngine.declare(Beneficiary())
 inferenceEngine.run()
+
